a97f5da7b374ff891c303066b8a7c88ab8ff2383
Daniel Li <dan@danyll.com>
2016-02-15 01:30:58 +0800

    Added description to package

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'brewhk:crate',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Flexible shopping cart package for Meteor',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
